93480dce6a53b015a0a66911b12b0fe9359546f5
Ibrohim229 <Botirjonov22@gmail.com>
2024-08-29 22:33:28 +0500

    updated

--- index.ts
+++ index.ts
@@ -1,28 +1,39 @@
+import icon1 from "@/assets/icons/about-us-advanteges-1.svg";
+import icon2 from "@/assets/icons/about-us-advanteges-2.svg";
+import icon3 from "@/assets/icons/about-us-advanteges-3.svg";
+import icon4 from "@/assets/icons/about-us-advanteges-4.svg";
+
+import img1 from "@/assets/equipment-slide-img-1.jpg";
+import img2 from "@/assets/equipment-slide-img-2.jpg";
+import img3 from "@/assets/equipment-slide-img-3.jpg";
+import img4 from "@/assets/equipment-slide-img-4.jpg";
+import img5 from "@/assets/equipment-slide-img-5.jpg";
+
 export const aboutInfo = [
   {
     id: 1,
-    icon: "/src/assets/icons/about-us-advanteges-1.svg",
+    icon: icon1,
     title: "Опыт и экспертиза",
     description:
       "Наша команда состоит из опытных специалистов, имеющих глубокие знания о технических особенностях Zeekr. Мы знаем, как обеспечить качественный ремонт и максимальную производительность вашего автомобиля.",
   },
   {
     id: 2,
-    icon: "/src/assets/icons/about-us-advanteges-2.svg",
+    icon: icon2,
     title: "Персонализированный подход",
     description:
       "Мы понимаем, что каждый клиент уникален, поэтому мы предлагаем индивидуальный подход к каждому ремонту. Мы внимательно слушаем ваши потребности и стремимся превзойти ваши ожидания.",
   },
   {
     id: 3,
-    icon: "/src/assets/icons/about-us-advanteges-3.svg",
+    icon: icon3,
     title: "Конкурентные цены",
     description:
       "Мы предлагаем конкурентные цены на наши услуги ремонта Zeekr. Мы стремимся сделать ремонт доступным для всех, не снижая при этом качество обслуживания.",
   },
   {
     id: 4,
-    icon: "/src/assets/icons/about-us-advanteges-4.svg",
+    icon: icon4,
     title: "Быстрый и удобный сервис",
     description:
       "Мы ценим ваше время, поэтому стараемся проводить ремонт Zeekr быстро и эффективно. Мы также предоставляем удобный сервис, чтобы вы могли легко и комфортно получить необходимые услуги.",
@@ -32,35 +43,35 @@ export const aboutInfo = [
 export const toolsInfo = [
   {
     id: 1,
-    img: "/src/assets/equipment-slide-img-1.jpg",
+    img: img1,
     title: "Подъемный стол",
     description:
       "Специальный подъемный стол используется для снятия/установки и дальнейшего ремонта высоковольтных батарей.",
   },
   {
     id: 2,
-    img: "/src/assets/equipment-slide-img-2.jpg",
+    img: img2,
     title: "Подъемный стол",
     description:
       "Специальный подъемный стол используется для снятия/установки и дальнейшего ремонта высоковольтных батарей.",
   },
   {
     id: 3,
-    img: "/src/assets/equipment-slide-img-3.jpg",
+    img: img3,
     title: "Подъемный стол",
     description:
       "Специальный подъемный стол используется для снятия/установки и дальнейшего ремонта высоковольтных батарей.",
   },
   {
     id: 4,
-    img: "/src/assets/equipment-slide-img-4.jpg",
+    img: img4,
     title: "Подъемный стол",
     description:
       "Специальный подъемный стол используется для снятия/установки и дальнейшего ремонта высоковольтных батарей.",
   },
   {
     id: 5,
-    img: "/src/assets/equipment-slide-img-5.jpg",
+    img: img5,
     title: "Подъемный стол",
     description:
       "Специальный подъемный стол используется для снятия/установки и дальнейшего ремонта высоковольтных батарей.",
